c77d38cf423ea4a531ddc3a04cdf289e9ef46548
murpiter <murpiter@gmail.com>
2023-02-17 01:41:13 +0400

    remove django templates and non-rest views

--- core/urls.py
+++ core/urls.py
@@ -6,42 +6,29 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path("", views.RoomsView.as_view(), name="rooms"),
-    path("authorize/", views.AuthorizeView.as_view(), name="authorize"),
-    path("register/", views.RegisterView.as_view(), name="register"),
-    path("room/<int:room_id>/", views.RoomDetailView.as_view(), name="room"),
+    path("rooms/", views.RoomListAPIView.as_view(), name="api_rooms"),
     path(
-        "user_reservations/", views.UserReservationsView.as_view(), name="reservations"
+        "rooms/<int:pk>", views.RoomDetailAPIView.as_view(), name="api_rooms_detail"
     ),
     path(
-        "logout/",
-        LogoutView.as_view(),
-        {"next_page": settings.LOGOUT_REDIRECT_URL},
-        name="logout",
-    ),
-    path("api/rooms/", views.RoomListAPIView.as_view(), name="api_rooms"),
-    path(
-        "api/rooms/<int:pk>", views.RoomDetailAPIView.as_view(), name="api_rooms_detail"
-    ),
-    path(
-        "api/reservations/",
+        "reservations/",
         views.ReservationListAPIView.as_view(),
         name="api_reservations",
     ),
     path(
-        "api/reservations/create/",
+        "reservations/create/",
         views.ReservationCreateAPIView.as_view(),
         name="api_reservations_create",
     ),
     path(
-        "api/reservations/<int:pk>/delete/",
+        "reservations/<int:pk>/delete/",
         views.ReservationDeleteAPIView.as_view(),
         name="api_reservations_delete",
     ),
     path(
-        "api/user/register/",
+        "user/register/",
         views.UserRegisterAPIView.as_view(),
         name="api_user_register",
     ),
-    path("api/user/obtain_token/", obtain_auth_token, name="api_obtain_token"),
+    path("user/obtain_token/", obtain_auth_token, name="api_obtain_token"),
 ]
--- room_reservation/urls.py
+++ room_reservation/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('api/', include('core.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
